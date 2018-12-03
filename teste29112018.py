fce1e436274ac1d275af02131aa87f236bfbb387
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-12-03 17:52:00 -0200

    Mais algumas melhorias no código-fonte feito em 03/12/2018.

--- teste29112018.py
+++ teste29112018.py
@@ -5,6 +5,7 @@ def get_dados():
     return [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
 
 
+# Para cada elemento retorna os outros elementos alcançáveis via um salto simples do cavalo!
 def get_elem_dic():
     return {1 : (6, 8), 2 : (7, 9),
             3 : (4, 8), 4 : (3, 9),
@@ -26,10 +27,13 @@ def f_aux(listas, n):
     for lista in listas:
         novas_pos_s = get_available_positions(lista[-1])
         if novas_pos_s:
-            lista1, lista2 = lista[:], lista[:]
+            lista1, lista2 = lista[:], lista
             lista1.append(novas_pos_s[0])
             lista2.append(novas_pos_s[1])
             result.extend([lista1, lista2])
+
+    if not result:
+        return result
 
     return f_aux(result, n)
 
@@ -45,5 +49,5 @@ if __name__ == "__main__":
     p = int(input('Insira p: '))
     n = int(input('Insira n: '))
     r = f(p, n)
-    print 'len(f):', len(r)
+    # print 'len(f):', len(r)
     print 'resultado:', r
