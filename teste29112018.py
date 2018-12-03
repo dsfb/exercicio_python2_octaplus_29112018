2b10ba470933626bafa1a68dce184005d19f3a44
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-12-03 17:34:47 -0200

    Novo código-fonte para o dia 03/12/2018.

--- teste29112018.py
+++ teste29112018.py
@@ -13,11 +13,8 @@ def get_elem_dic():
             9 : (2, 4)}
 
 
-elem_dic = get_elem_dic()
-
-
-def get(pc):
-    return elem_dic[pc]
+def get_available_positions(pc):
+    return get_elem_dic()[pc]
 
 
 def f_aux(listas, n):
@@ -27,48 +24,26 @@ def f_aux(listas, n):
 
     result = list()
     for lista in listas:
-        p = lista[-1]
-        novas_pos_s = get(p)
-        new_lst_s = lista * len(novas_pos_s)
-
-        iterator = 0
-        for indice, el in enumerate(novas_pos_s):
-            for i in range(iterator, iterator + len(lista)):
-                new_lst_s[i].append(el)
-            iterator += len(novas_pos_s)
-        result.extend(new_lst_s)
+        novas_pos_s = get_available_positions(lista[-1])
+        if novas_pos_s:
+            lista1, lista2 = lista[:], lista[:]
+            lista1.append(novas_pos_s[0])
+            lista2.append(novas_pos_s[1])
+            result.extend([lista1, lista2])
 
     return f_aux(result, n)
 
 
 def f(p, n):
-    result = list()
     if p > 0 and p < 10 and n > 0:
         return f_aux([[p,],], n)
 
-    return result
+    return list()
 
 
 if __name__ == "__main__":
-    assert 8 in get(1)
-    assert 6 in get(1)
-    assert 7 in get(2)
-    assert 9 in get(2)
-    assert 8 in get(3)
-    assert 4 in get(3)
-    assert 9 in get(4)
-    assert 3 in get(4)
-    assert not len(get(5))
-    assert 7 in get(6)
-    assert 1 in get(6)
-    assert 6 in get(7)
-    assert 2 in get(7)
-    assert 1 in get(8)
-    assert 3 in get(8)
-    assert 4 in get(9)
-    assert 2 in get(9)
     p = int(input('Insira p: '))
     n = int(input('Insira n: '))
     r = f(p, n)
     print 'len(f):', len(r)
-    # print 'resultado:', r
+    print 'resultado:', r
