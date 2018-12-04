85ef96d1efed8b0b771b888bca73c27e446a1c5c
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-12-04 13:30:16 -0200

    Commit representativo da implementação final para a manhã de 04/12/2018.

--- teste29112018.py
+++ teste29112018.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import cProfile
 
 def get_dados():
     return [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
 
 
-# Para cada elemento retorna os outros elementos alcançáveis via um salto simples do cavalo!
-def get_elem_dic():
-    return {1 : (6, 8), 2 : (7, 9),
+# Para cada elemento, mapeia os outros elementos alcançáveis via um salto simples do cavalo!
+elem_dic = {1 : (6, 8), 2 : (7, 9),
             3 : (4, 8), 4 : (3, 9),
             5 : tuple(), 6 : (1, 7),
             7 : (2, 6), 8 : (1, 3),
@@ -15,7 +15,7 @@ def get_elem_dic():
 
 
 def get_available_positions(pc):
-    return get_elem_dic()[pc]
+    return elem_dic[pc]
 
 
 def f_aux(listas, n):
@@ -24,22 +24,24 @@ def f_aux(listas, n):
         return listas
 
     result = list()
+
     for lista in listas:
         novas_pos_s = get_available_positions(lista[-1])
-        if novas_pos_s:
-            lista1, lista2 = lista[:], lista
-            lista1.append(novas_pos_s[0])
-            lista2.append(novas_pos_s[1])
-            result.extend([lista1, lista2])
+        lista1 = lista[:]
+        lista1.append(novas_pos_s[0])
+        lista.append(novas_pos_s[1])
+        result.append(lista1)
 
-    if not result:
-        return result
+    listas.extend(result)
 
-    return f_aux(result, n)
+    return f_aux(listas, n)
 
 
 def f(p, n):
     if p > 0 and p < 10 and n > 0:
+        if p == 5:
+            return list()
+
         return f_aux([[p,],], n)
 
     return list()
@@ -49,5 +51,8 @@ if __name__ == "__main__":
     p = int(input('Insira p: '))
     n = int(input('Insira n: '))
     r = f(p, n)
-    # print 'len(f):', len(r)
-    print 'resultado:', r
+
+    cProfile.run('s = f(p, n)')
+
+    print 'len(f):', len(r)
+    # print 'resultado:', r
